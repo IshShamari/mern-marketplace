88e6762be5006bad67b19082a0fa1c44dc7067a9
Shamari Ishmael <shamariishmael@gmail.com>
2023-08-25 15:10:28 -0600

    Created auth middleware

---
+++ microservices/user/middlewares/authMiddleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { IDecodedToken } from '../models/interfaces';
+
+export const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
+    // Extract the token from the header
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({message: 'Unauthorized'});
+    }
+
+    const token = authHeader.split(' ')[1]; // "Bearer <token> format"
+
+    // Verify the token
+    jwt.verify(token, process.env.JWT_SECRET as string, (err, decodedToken: any) => {
+        if (err) {
+            return res.status(403).json({message: 'Forbidden'});
+        }
+        
+        const userPayload = decodedToken as IDecodedToken;
+        req.userId = userPayload?.id;
+
+        next();
+    })
+}
