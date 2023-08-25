c23e8e0c41f1e85dc9ac736ee73712ccc9de712f
Shamari Ishmael <shamariishmael@gmail.com>
2023-08-25 15:10:58 -0600

    Created user routes and controller for those routes

---
+++ microservices/user/routes/userRoutes.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { authenticateJWT } from '../middlewares/authMiddleware';
+import * as userController from '../controllers/userControllers';
+
+const router = express.Router();
+
+router.post('/register', userController.registerUser);
+
+router.post('/login', userController.loginUser);
+
+router.get('/me', authenticateJWT, userController.getMe);
+
+export default router;
