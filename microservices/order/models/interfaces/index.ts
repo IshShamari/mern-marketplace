887ff9ba13471c7fa96cdaa0c9a4081aa23eecce
Shamari Ishmael <shamariishmael@gmail.com>
2023-08-25 18:53:27 -0600

    Created order model and its interfaces

---
+++ microservices/order/models/interfaces/index.ts
@@ -0,0 +1,9 @@
+import IOrderItem from './IOrderItem';
+import IOrder from './IOrder';
+import IOrderDocument from './IOrderDocument';
+
+export {
+    IOrder,
+    IOrderItem,
+    IOrderDocument
+}
