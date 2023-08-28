277c20753c90e7f47529032eb7956a561cd80c1a
Shamari Ishmael <shamariishmael@gmail.com>
2023-08-27 23:23:29 -0600

    Split server.ts into app.ts and server.ts

--- microservices/user/server.ts
+++ microservices/user/server.ts
@@ -1,31 +1,25 @@
-import express from 'express';
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes';
-import { Request, Response, NextFunction } from 'express';
+import app from './app';
 
 dotenv.config();
 
-const app = express();
+
 const PORT = process.env.USER_SERVICE_PORT || 3001;
-const MONGO_URL = process.env.MONGO_URI || '';
+console.log(process.env.NODE_ENV);
+const MONGO_URI = process.env.NODE_ENV === 'test' ? 'mongodb://localhost:27017/' : process.env.MONGO_URI || '';
 
-app.use(express.json());
+console.log(MONGO_URI);
 
-/*mongoose.connect(MONGO_URL).then(() => {
+mongoose.connect(MONGO_URI, {family: 4}).then(() => {
     console.log('Connected to MongoDB');
+
+    mongoose.connection.useDb('user');
+
+    app.listen(PORT, () => {
+        console.log(`User Service is running on port ${PORT}`);
+    })
+    
 }).catch(err => {
     console.error('MongoDB connection error:', err);
-})*/
-
-// Routes
-app.use('/api/users', userRoutes);
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
-    res.status(500).send('Internal server error');
-});
-
-app.listen(PORT, () => {
-    console.log(`User Service is running on port ${PORT}`);
 })
