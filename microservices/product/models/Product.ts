500919d47dab5e2643cda7daa5b9cb31b23fea90
Shamari Ishmael <shamariishmael@gmail.com>
2023-08-28 21:17:58 -0600

    Containerized product microservice

--- microservices/product/models/Product.ts
+++ microservices/product/models/Product.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Document } from 'mongoose';
+import { Schema, model } from 'mongoose';
 import { IProductDocument } from './interfaces';
 
 const productSchema = new Schema({
